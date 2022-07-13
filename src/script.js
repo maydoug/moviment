a711c61e3fee8d5aca63bf9983049d0b6e406302
unknown <maydoug@hotmail.com>
2022-07-13 17:49:30 -0300

    primeiro commit landing page

---
+++ src/script.js
@@ -0,0 +1,27 @@
+async function fazPost(body){
+    fetch('https://api-freedevs.herokuapp.com/lead', { 
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body),
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    
+    console.log(body)
+    }
+
+function cadastraUsuario(){
+event.preventDefault()
+const emailElemet = document.getElementById('email').value
+
+body = {
+    "email": emailElemet,
+    }
+
+    fazPost(body);
+    return
+}
+
+window.location.reload('./sucess.html');
